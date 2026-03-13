7cd5466ca49eaf663c7adbf5b3bfe8e12595edaf
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-13 13:13:02 +0100

    the addtod function has been implemetend

--- config/firebase.js
+++ config/firebase.js
@@ -14,3 +14,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const githubProvider = new GithubAuthProvider();
+
+console.log("✅ Firebase initialized:", app.options.projectId);
